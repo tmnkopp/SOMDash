472d4d2dd043421afd54294328af3aec6276c93c
Tim Kopp <tim.kopp@cyber-balance.com>
2020-06-01 10:35:26 -0400

    refactor compiler

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ConstantsService{
     readonly baseApiUrl: string = 'http://somapi/api/'; //
-    //readonly baseApiUrl: string = 'http://localhost:4000/api/';
+    //  readonly baseApiUrl: string = 'http://localhost:4000/api/';
  
 }
